5847517f86ffe8e08ee6c3c6512ec589a7d657e6
AlexanderVorobyev <passer.alexander@gmail.com>
2022-07-25 14:30:30 +0700

    add: DomainResult

---
+++ MyFavoriteMovie/Core/DomainResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class DomainResult
+    {
+        public string? Message { get; }
+        public bool Succes { get; }
+
+
+        public DomainResult()
+        {
+            Succes = true;
+        }
+
+        public DomainResult(string message)
+        {
+            Message = message;
+        }
+
+
+        public static DomainResult Succeeded() => new();
+        public static DomainResult Failed(string message) => new(message);
+    }
+
+    public sealed class DomainResult<T> : DomainResult
+    {
+        public T? Result { get; }
+
+        public DomainResult(T result) => Result = result;
+        private DomainResult(string message) : base(message) { }
+
+        public static DomainResult<T> Succeeded(T result) => new(result);
+        public static new DomainResult<T> Failed(string message) => new(message);
+    }
+}
