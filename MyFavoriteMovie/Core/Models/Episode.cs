4f7ff50b27c96f9bceee768f31a8c1da6e336f14
AlexanderVorobyev <passer.alexander@gmail.com>
2022-07-25 13:15:44 +0700

    add: Episode

---
+++ MyFavoriteMovie/Core/Models/Episode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Episode
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string? Name { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Season { get; set; }
+        public TimeSpan? Duration { get; set; }
+        public DateTime? RealeseDate { get; set; }
+    }
+}
