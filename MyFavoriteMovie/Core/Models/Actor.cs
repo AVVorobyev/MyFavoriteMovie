c6e4747dc157bc33e3a63856a5a99893e4ae9c44
AlexanderVorobyev <passer.alexander@gmail.com>
2022-07-25 13:12:27 +0700

    add: Actor

---
+++ MyFavoriteMovie/Core/Models/Actor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string? Name { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string? Surname { get; set; }
+
+        public double? HeightInMeters { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? DeathDate { get; set; }
+    }
+}
