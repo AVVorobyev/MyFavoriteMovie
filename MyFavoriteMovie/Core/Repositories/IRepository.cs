c736bf2f0205d57078d3e0577e4aa7d5490541ad
AlexanderVorobyev <passer.alexander@gmail.com>
2022-07-25 15:34:38 +0700

    add: IRepository

---
+++ MyFavoriteMovie/Core/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        public Task<DomainResult<T>> GetByIdAsync(Expression<Func<T, bool>> filter, string? includeProperties);
+        public Task<DomainResult<IEnumerable<T>>> GetAsync(int skip, int take);
+        public Task<DomainResult> AddAsync(T entity);
+        public Task<DomainResult> UpdateAsync(T entity);
+        public Task<DomainResult> DeleteAsync(T entity);
+    }
+}
