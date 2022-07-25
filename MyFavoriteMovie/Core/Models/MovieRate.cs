e76e9de84280eb425f44f034990dc25b53d796b3
AlexanderVorobyev <passer.alexander@gmail.com>
2022-07-25 13:22:29 +0700

    add: MovieRate

---
+++ MyFavoriteMovie/Core/Models/MovieRate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class MovieRate
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(10)]
+        public byte Rate { get; set; }
+    }
+}
