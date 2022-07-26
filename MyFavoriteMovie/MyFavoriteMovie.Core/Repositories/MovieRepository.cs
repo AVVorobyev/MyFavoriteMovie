a017410ca0fe7b19272aedba78c1adf1dc414b18
AlexanderVorobyev <passer.alexander@gmail.com>
2022-07-26 19:42:36 +0700

    add: MovieRepository

---
+++ MyFavoriteMovie/MyFavoriteMovie.Core/Repositories/MovieRepository.cs
@@ -0,0 +1,18 @@
+﻿using MyFavoriteMovie.Core.Contexts;
+using MyFavoriteMovie.Core.Models;
+using MyFavoriteMovie.Core.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFavoriteMovie.Core.Repositories
+{
+    public class MovieRepository : Repository<Movie>, IMovieRepository
+    {
+        public MovieRepository(MSSQLDbContext context) : base(context)
+        {
+        }
+    }
+}
