c154aeb2a9f6b1811c389079060b2aa1d61b72f8
AlexanderVorobyev <passer.alexander@gmail.com>
2022-07-26 19:47:57 +0700

    add: ActorRepository

---
+++ MyFavoriteMovie/MyFavoriteMovie.Core/Repositories/ActorRepository.cs
@@ -0,0 +1,20 @@
+﻿using MyFavoriteMovie.Core.Contexts;
+using MyFavoriteMovie.Core.Models;
+using MyFavoriteMovie.Core.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFavoriteMovie.Core.Repositories
+{
+    public class ActorRepository : Repository<Actor>, IActorRepository
+    {
+        public ActorRepository(MSSQLDbContext context) : base(context)
+        {
+        }
+
+
+    }
+}
---
+++ MyFavoriteMovie/MyFavoriteMovie.Core/Repositories/Interfaces/IActorRepository.cs
@@ -0,0 +1,13 @@
+﻿using MyFavoriteMovie.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFavoriteMovie.Core.Repositories.Interfaces
+{
+    public interface IActorRepository : IRepository<Actor>
+    {
+    }
+}
