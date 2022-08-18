b5d223ac9c8ef129f90f3ac5d06023b19f56a256
AlexanderVorobyev <passer.alexander@gmail.com>
2022-08-18 19:01:02 +0700

    add: EditMovieModal

---
+++ MyFavoriteMovie/react-client/src/views/Movie/EditMovieModal.js
@@ -0,0 +1,81 @@
+import axios from "axios";
+import React, { Component } from "react";
+import { Button, Form, Modal, } from "react-bootstrap";
+
+export class EditMovieModal extends Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        axios({
+            method: 'PUT',
+            url: process.env.REACT_APP_API_URL_Movie + "Update",
+            params: {
+                Id: e.target.Id.value,
+                Name: e.target.Name.value,
+                Poster: e.target.Poster.value
+            }
+        }).then(response => {
+            alert(response.data);
+            this.refreshPage();
+        }, (error) => {
+            alert("error!");
+        });
+    }
+
+    refreshPage() {
+        window.location.reload();
+    }
+
+    render() {
+        return (
+            <div className='conteiner'>
+                <Modal
+                    {...this.props}>
+                    <Modal.Header closeButton>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            Edit Movie
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Group controlId="Id">
+                                <Form.Label>Id</Form.Label>
+                                <Form.Control type="text" name="Id" required
+                                    disabled
+                                    defaultValue={this.props.Id}
+                                    placeholder="Id" />
+                            </Form.Group>
+
+                            <Form.Group controlId="Name">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="text" name="Name" required
+                                    defaultValue={this.props.Name}
+                                    placeholder="Name" />
+                            </Form.Group>
+
+                            <Form.Group controlId="Poster">
+                                <Form.Label>Poster</Form.Label>
+                                <Form.Control type="text" name="Poster"
+                                    defaultValue={this.props.Poster}
+                                    placeholder="Poster" />
+                            </Form.Group>
+                            
+                            <Form.Group>
+                                <Button variant="primary" type="submit">
+                                    Update
+                                </Button>
+                            </Form.Group>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.props.onHide}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        )
+    }
+}
--- MyFavoriteMovie/react-client/src/views/Movie/Movie.js
+++ MyFavoriteMovie/react-client/src/views/Movie/Movie.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
+import { Button, ButtonToolbar } from "react-bootstrap";
 import { NavLink, Navigate } from "react-router-dom";
+import { EditMovieModal } from "./EditMovieModal";
 
 export class Movie extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { movie: Object, redirect: false };
+        this.state = { movie: Object, redirect: false, editMovieShow: false };
     }
 
     setRedirect() {
@@ -35,7 +36,7 @@ export class Movie extends Component {
     componentDidMount() {
         this.getMovieById();
     }
-    
+
     deleteMovie(movie) {
         console.log(movie);
         if (window.confirm('Are you sure?')) {
@@ -48,8 +49,6 @@ export class Movie extends Component {
             }).then(response => {
                 alert(response.data);
                 this.setRedirect();
-
-
             }, (error) => {
                 alert("Error!");
             });
@@ -62,17 +61,29 @@ export class Movie extends Component {
             <div>
                 {this.renderRedirect()}
                 <h1>
-                    Movie page MovieId = {movie.Id}
+                    Movie page
+                    <h2>MovieId = {movie.Id}</h2>
                     <h4>
                         <h5 class="text-success">{movie.Name}</h5>
                         <h5 class="text-success">{movie.Poster}</h5>
                         <h5 class="text-success">{movie.RealeseDate}</h5>
                     </h4>
                 </h1>
-                <NavLink to="/Movie/Movies" className="btn btn-primary">Back</NavLink>
-                <Button
-                    onClick={() => { this.deleteMovie(movie);  }}
-                    variant='danger'>Delete</Button>
+                <ButtonToolbar>
+                    <NavLink to="/Movie/Movies" className="btn btn-primary">Back</NavLink>
+                    <Button onClick={() => this.setState({ editMovieShow: true, movie: movie })}
+                        variant='info'>Edit</Button>
+                    <Button
+                        onClick={() => { this.deleteMovie(movie); }}
+                        variant='danger'>Delete</Button>
+
+                    <EditMovieModal show={this.state.editMovieShow}
+                        onHide={() => this.setState({ editMovieShow: false })}
+                        Id={movie.Id}
+                        Name={movie.Name}
+                        Poster={movie.Poster}>
+                    </EditMovieModal>
+                </ButtonToolbar>
             </div>
         )
     }
